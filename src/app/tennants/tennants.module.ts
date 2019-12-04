b9a16adf8d738aa7f18eec9f0b9be945848229e7
AndreT503 <atournier3@msn.com>
2019-12-04 12:14:10 -0800

    check prior to update

--- src/app/tennants/tennants.module.ts
+++ src/app/tennants/tennants.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
-import { TennantsRoutingModule } from './tennants-routing.module';
-import { ProfileComponent } from './profile/profile.component';
-import { LogoutComponent } from './logout/logout.component';
+import { TennantsRoutingModule } from './tennants-routing.module'
+import { ProfileComponent } from './profile/profile.component'
+import { LogoutComponent } from './logout/logout.component'
 import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component'
 
 @NgModule({
